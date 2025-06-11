80276a7ac8d69a572b338c3cabeadcc7fc207295
SriGowtham <92846204+SriGowtham@users.noreply.github.com>
2025-06-11 21:03:44 +0530

    feat: Side nav navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterOutlet } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule,MatIconModule, RouterOutlet],
+  imports: [MatButtonModule,MatIconModule, RouterOutlet,MatSidenavModule, MatToolbarModule,RouterLink, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
